4127746813647820609b8a55a3c92dd8ed536481
Dimitris <zdimitris@outlook.com>
2021-07-05 20:07:19 +0300

    Barcode scan approach

--- js/medicine_list.js
+++ js/medicine_list.js
@@ -295,6 +295,11 @@ function showCreateMedicineSection() {
     barcodeInput.classList.add('info-textarea');
     barcodeInput.placeholder = 'Barcode';
     barcodeInput.id = 'medicine-barcode-txt';
+    var barcodeScanButton = document.createElement('input');
+    barcodeScanButton.setAttribute('type', 'button');
+    barcodeScanButton.id = 'barcode-scan-btn';
+    barcodeScanButton.innerHTML = 'Scan';
+    barcodeScanButton.addEventListener('click', startScanner);
 
     /* Medicine Remarks */
     var remarksTextArea = document.createElement('textarea');
@@ -326,20 +331,101 @@ function showCreateMedicineSection() {
     buttonDiv.appendChild(okImg);
     buttonDiv.appendChild(cancelImg);
     newMedicineDiv.appendChild(nameTextArea);
-    newMedicineDiv.appendChild(document.createElement('br'));
     newMedicineDiv.appendChild(expirationDateInput);
-    newMedicineDiv.appendChild(document.createElement('br'));
     newMedicineDiv.appendChild(medicineStockCountInput);
-    newMedicineDiv.appendChild(document.createElement('br'));
     newMedicineDiv.appendChild(barcodeInput);
-    newMedicineDiv.appendChild(document.createElement('br'));
+    newMedicineDiv.appendChild(barcodeScanButton);
     newMedicineDiv.appendChild(remarksTextArea);
-    newMedicineDiv.appendChild(document.createElement('br'));
     newMedicineDiv.appendChild(buttonDiv);
     document.getElementById('add-medicine-img').parentNode.insertBefore(newMedicineDiv, document.getElementById('add-medicine-img'));
     setTimeout(function () {
         newMedicineDiv.classList.add('visible');
     }, 50);
+}
+
+var _scannerIsRunning = false;
+
+function startScanner() {
+    Quagga.init({
+        inputStream: {
+            name: 'Live',
+            type: 'LiveStream',
+            target: document.querySelector('#scanner-container'),
+            constraints: {
+                width: 480,
+                height: 320,
+                facingMode: 'environment'
+            },
+        },
+        decoder: {
+            readers: [
+                'code_128_reader',
+                'ean_reader',
+                'ean_8_reader',
+                'code_39_reader',
+                'code_39_vin_reader',
+                'codabar_reader',
+                'upc_reader',
+                'upc_e_reader',
+                'i2of5_reader'
+            ],
+            debug: {
+                showCanvas: true,
+                showPatches: true,
+                showFoundPatches: true,
+                showSkeleton: true,
+                showLabels: true,
+                showPatchLabels: true,
+                showRemainingPatchLabels: true,
+                boxFromPatches: {
+                    showTransformed: true,
+                    showTransformedBox: true,
+                    showBB: true
+                }
+            }
+        },
+
+    }, function (err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        console.log('Initialization finished. Ready to start');
+        Quagga.start();
+
+        // Set flag to is running
+        _scannerIsRunning = true;
+    });
+
+    Quagga.onProcessed(function (result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute('width')), parseInt(drawingCanvas.getAttribute('height')));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: 'green', lineWidth: 2 });
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: '#00F', lineWidth: 2 });
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+            }
+        }
+    });
+
+
+    Quagga.onDetected(function (result) {
+        console.log('Barcode detected and processed : [' + result.codeResult.code + ']', result);
+    });
 }
 
 function getProfileInstance() {
