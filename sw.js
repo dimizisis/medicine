3a4153842651b880cf6f667f8e74c02bcef53242
Dimitris <zdimitris@outlook.com>
2021-07-07 15:44:50 +0300

    Fixed sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
+    console.log('The service worker is being installed.');
     event.waitUntil(
-        caches.open('medicine').then(function (cache) {
+        caches.open('medicine').then(function(cache) {
             return cache.addAll([
                 './index.html',
                 './404.html',
@@ -43,9 +44,10 @@ self.addEventListener('install', function (event) {
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
+    console.log('The service worker is serving the asset.');
     event.respondWith(
-        caches.match(event.request).then(function (response) {
+        caches.match(event.request).then(function(response) {
             return response || caches.match('/medicine/index.html');
         })
     );
