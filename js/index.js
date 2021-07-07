62f8d42a9244b2525e823a8ba50e48eb916bf4e3
Dimitris <zdimitris@outlook.com>
2021-07-07 18:11:38 +0300

    Updated href

--- js/index.js
+++ js/index.js
@@ -91,7 +91,7 @@ function createProfileElement(profile) {
     profileNameLabel.classList.add('create-profile-labels');
     profileNameLabel.classList.add('profile-name');
     profileNameLabel.innerHTML = profile.name;
-    // profileNameLabel.href = 'medicine_list.html?profile=' + profile.name;
+    profileNameLabel.href = 'medicine_list.html?profile=' + profile.name;
     profileNameLabel.addEventListener('click', function (e) {
         e.preventDefault();
         setTimeout(function(){document.location.href = 'medicine_list.html?profile=' + profile.name;},500);
